17f0c49f47beb549c2e2664fc5df34537b6f954f
mthigh2021 <khadkadipak32@gmail.com>
2021-05-17 17:42:45 -0400

    first commit

---
+++ labWork/lab1/src/main/ConverisonMethods.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.util.Scanner;
+
+public class ConverisonMethods {
+import java.util.Scanner;
+	
+	
+	public static void main (String[] args) {
+		
+		 int menuSelection;
+		 Scanner myScanner = new Scanner(System.in);
+		 //String line = myscanner.nextLine();
+		 System.out.println("Please choose a number and hit Enter.");
+		 System.out.println("1. Cups to Teaspoons");
+		 System.out.println("2. Miles to Kilometers");
+		 System.out.println("3. US Gallons to Imperial Gallons");
+		 System.out.println("4. Quit");
+		
+		
+	}
+public void int collectInput(int a) {
+	return a;
+}
+	
+	
+ public void cupToteaSpoon() {
+	 
+	 
+	 
+ }
+public void   mileToKiloMeter() {
+	
+	
+}
+public void galonToImperialSystem() {
+	 
+	
+}
+
+}
---
+++ src/main/java/day2/operator.java
@@ -0,0 +1,36 @@
+package day2;
+
+public class operator {
+
+	public static void main(String[] args)
+	{
+	//
+	//System.out.println("hello world");
+	
+	  int x = 3;
+	  int y = 4;
+	  
+	  int z = x + y;
+	  
+	  System.out.println(z);
+	//  System.out.println(((double)x / (double) y);
+	  System.out.println(x /(double) y);
+	  System.out.println();
+	  System.out.println();
+
+	//using control statements 
+	  if (x%2 == 0) {
+		  System.out.println(x + " is and even number");
+	  }
+	  else {
+		  System.out.println(x + "is a odd number");
+	  }
+	  
+	  
+	//long casteLng = 941;
+	//int casteint = (int)  casteLng;
+	
+	//System.out.println("Casted INt" + casteint);
+	//System.out.println("Casted LNG" + casteLng);
+	}
+}
